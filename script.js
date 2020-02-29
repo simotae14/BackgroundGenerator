6a92e5d4bdc50825625409f602fc309cc2722008
simotae14 <startae14@gmail.com>
2020-02-29 18:50:27 +0100

    BONUS: add random color generator button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomButton = document.querySelector(".random");
 
 function setGradient() {
     body.style.background = "linear-gradient(to right, "
@@ -12,6 +13,18 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+function generateRandomColor () {
+    const randomColor ='#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+};
+
+function generateRandomHEX() {
+    color1.value = generateRandomColor();
+    color2.value = generateRandomColor();
+    setGradient();
+}
+
+color1.addEventListener("change", setGradient);
+color2.addEventListener("change", setGradient);
 window.addEventListener("load", setGradient);
+randomButton.addEventListener("click", generateRandomHEX);
